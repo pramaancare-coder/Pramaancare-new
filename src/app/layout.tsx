3b32e546c021b2354042a69f5db034173becccb1
Tarun Dayal <tarundayal@Taruns-MacBook-Air.local>
2026-01-15 23:08:10 +0530

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -213,13 +213,17 @@ export default function RootLayout({
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-T6DJKQMVB7"
           strategy="afterInteractive"
+          defer
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-T6DJKQMVB7');
+            gtag('config', 'G-T6DJKQMVB7', {
+              page_title: document.title,
+              page_location: window.location.href,
+            });
           `}
         </Script>
 
